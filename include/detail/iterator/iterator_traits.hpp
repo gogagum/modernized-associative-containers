7be89c2c9f89ed1bce7e6d01805a46b8154bb82b
Georgy Guminov <gogagum@gmail.com>
2026-01-24 22:48:16 +0300

    Renames and reformatting.

--- include/detail/iterator/iterator_traits.hpp
+++ include/detail/iterator/iterator_traits.hpp
@@ -329,19 +329,19 @@ using __has_exactly_bidirectional_iterator_category =
                       __has_iterator_category_convertible_to<_Tp, bidirectional_iterator_tag>::value &&
                           !__has_iterator_category_convertible_to<_Tp, random_access_iterator_tag>::value>;
 
-template <class _InputIterator>
-using __iterator_value_type = typename iterator_traits<_InputIterator>::value_type;
+template <class InputIteratorT>
+using __iterator_value_type = typename iterator_traits<InputIteratorT>::value_type;
 
-template <class _InputIterator>
-using __iter_key_type = std::remove_const_t<std::tuple_element_t<0, __iterator_value_type<_InputIterator>>>;
+template <class InputIteratorT>
+using __iter_key_type = std::remove_const_t<std::tuple_element_t<0, __iterator_value_type<InputIteratorT>>>;
 
-template <class _InputIterator>
-using __iter_mapped_type = std::tuple_element_t<1, __iterator_value_type<_InputIterator>>;
+template <class InputIteratorT>
+using __iter_mapped_type = std::tuple_element_t<1, __iterator_value_type<InputIteratorT>>;
 
-template <class _InputIterator>
+template <class InputIteratorT>
 using __iter_to_alloc_type =
-    std::pair<const std::tuple_element_t<0, __iterator_value_type<_InputIterator>>,
-         std::tuple_element_t<1, __iterator_value_type<_InputIterator>>>;
+    std::pair<const std::tuple_element_t<0, __iterator_value_type<InputIteratorT>>,
+         std::tuple_element_t<1, __iterator_value_type<InputIteratorT>>>;
 
 template <class _Iter>
 using __iterator_iterator_category = typename iterator_traits<_Iter>::iterator_category;
