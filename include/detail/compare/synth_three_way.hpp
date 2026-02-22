c806edc5cef5b2421ae40093421e437a50e268ee
Georgy Guminov <gogagum@gmail.com>
2026-02-23 02:18:44 +0300

    Renames

--- include/detail/compare/synth_three_way.hpp
+++ include/detail/compare/synth_three_way.hpp
@@ -20,13 +20,13 @@ namespace mstd {
 
 // [expos.only.func]
 
-inline constexpr auto __synth_three_way = []<class _Tp, class _Up>(const _Tp& __t, const _Up& __u)
+inline constexpr auto __synth_three_way = []<class ValueT, class _Up>(const ValueT& __t, const _Up& __u)
   requires requires {
     { __t < __u } -> std::convertible_to<bool>;
     { __u < __t } -> std::convertible_to<bool>;
   }
 {
-  if constexpr (std::three_way_comparable_with<_Tp, _Up>) {
+  if constexpr (std::three_way_comparable_with<ValueT, _Up>) {
     return __t <=> __u;
   } else {
     if (__t < __u)
@@ -37,9 +37,9 @@ inline constexpr auto __synth_three_way = []<class _Tp, class _Up>(const _Tp& __
   }
 };
 
-template <class _Tp, class _Up = _Tp>
+template <class ValueT, class _Up = ValueT>
 using __synth_three_way_result =
-    decltype(mstd::__synth_three_way(std::declval<_Tp&>(), std::declval<_Up&>()));
+    decltype(mstd::__synth_three_way(std::declval<ValueT&>(), std::declval<_Up&>()));
 
 } // namespace mstd
 
