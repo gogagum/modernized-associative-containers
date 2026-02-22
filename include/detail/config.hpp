a8988061345e17c20d3d8e5e76eb036f07eceba4
Georgy Guminov <gogagum@gmail.com>
2026-02-23 02:14:02 +0300

    enable_if -> requires

--- include/detail/config.hpp
+++ include/detail/config.hpp
@@ -18,21 +18,6 @@
 #  define MSTD_CONCAT(_X, _Y) MSTD_CONCAT_IMPL(_X, _Y)
 #  define MSTD_CONCAT3(X, Y, Z) MSTD_CONCAT(X, MSTD_CONCAT(Y, Z))
 
-// There are a handful of public standard library types that are intended to
-// support CTAD but don't need any explicit deduction guides to do so. This
-// macro is used to mark them as such, which suppresses the
-// '-Wctad-maybe-unsupported' compiler warning when CTAD is used in user code
-// with these classes.
-#  ifdef MSTD_COMPILER_CLANG_BASED
-#    define MSTD_CTAD_SUPPORTED_FOR_TYPE(_ClassName)                                     \
-      template <class... _Tag>                                                           \
-      [[maybe_unused]] _ClassName(typename _Tag::__allow_ctad...)->_ClassName<_Tag...>
-#  else
-#    define MSTD_CTAD_SUPPORTED_FOR_TYPE(ClassName)                                      \
-      template <class... _Tag>                                                           \
-      ClassName(typename _Tag::__allow_ctad...)->ClassName<_Tag...>
-#  endif
-
 #endif // __cplusplus
 
 #endif // MSTD___CONFIG
